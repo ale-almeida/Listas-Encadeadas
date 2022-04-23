7aaffb1519772c08e36babdbab952682937bde90
Alessandro Almeida <90009634+ale-almeida@users.noreply.github.com>
2022-04-23 20:44:30 -0300

    Add files via upload

---
+++ Questoes.java
@@ -0,0 +1,14 @@
+
+public class Questoes {
+
+	public static void main(String[] args) {
+		ListaSimplismente<Integer> lista = new ListaSimplismente<>();
+		for (int i = 0; i < 5; i++) {
+			lista.adiciona(i);	
+			System.out.println(lista);
+		}
+		
+	
+	}
+
+}
